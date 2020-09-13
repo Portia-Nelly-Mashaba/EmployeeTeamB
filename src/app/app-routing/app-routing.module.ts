c5b06e7ff71b7a98764020e05ac883e30e3e1680
nelisiwem <pnpnelly@gmail.com>
2020-09-13 08:55:39 +0200

    2 files modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { EmployeeComponent } from '../employee/employee.component';
 import { DepartmentComponent } from '../department/department.component';
+import { ProjectListComponent } from '../project-list/project-list.component';
 
 const appRoutes :Routes =[
   {path:'', component:EmployeeComponent}, 
   {path:'employee', component:EmployeeComponent}, 
-  {path:'department', component:DepartmentComponent}];
+  {path:'department', component:DepartmentComponent}, {path:'project-list', component:ProjectListComponent}];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forRoot(appRoutes)],
