9fb2443f749e96e04111e6c6c3108963585bffb8
nelisiwem <pnpnelly@gmail.com>
2020-09-13 08:40:36 +0200

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,17 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+ import {MatIconModule} from '@angular/material/icon';
+
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule, MatCardModule,MatButtonModule,MatGridListModule, MatDialogModule, MatMenuModule],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, MatCardModule,MatButtonModule,MatGridListModule, MatDialogModule, MatMenuModule,MatFormFieldModule, MatInputModule,MatOptionModule, MatSelectModule, MatIconModule ],
 
   declarations: [ AppComponent, HelloComponent, EmployeeComponent, UpdateComponent, DepartmentComponent, MenuHeaderComponent, ProjectListComponent ],
 
